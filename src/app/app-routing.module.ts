7a9a09b8402e324d8654883073a37f60587ba56b
Yasodha Sundaresan <yasodhasundaresan@yasodhas-mbp.attlocal.net>
2020-03-28 15:24:37 -0400

    Initial loading page changed route!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import {HomeComponent}  from './core/home/home.component';
 import {RegisterComponent}  from './auth/register/register.component';
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent},
+  {path:'/',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent},
   {path:'wishlist',component:MyWishComponent,canActivate:[LoginGuard]},
