e25cd8fee8742964431b6bc0c1d5a841fd1534fb
Yasodha Sundaresan <yasodhasundaresan@yasodhas-mbp.attlocal.net>
2020-04-30 21:32:18 -0400

    Logic to login/logout on multiplt tabs added

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, DoCheck } from '@angular/core';
 import { AuthService } from 'src/app/auth/service/auth.service';
 import { Router } from '@angular/router';
 
@@ -13,10 +13,32 @@ export class HeaderComponent implements OnInit {
   constructor(private auth:AuthService,private router:Router) { }
 
   ngOnInit() {
+  
     this.auth.isLoggedInObservable.subscribe(value=>{
       this.isUserLoggedIn=value;
-    })
+        })
+     this.checkLocalStorage();  
   }
+
+    checkLocalStorage(){
+      //logic to login,logout on multiple tabs
+      window.addEventListener('storage', (event) => {
+        if (event.storageArea == localStorage) {
+          let token = localStorage.getItem('token');
+          if(token == undefined) { 
+            // you can update this as per your key
+              // DO LOGOUT FROM THIS TAB AS WELL
+              this.router.navigate(['/login']); // If you are using router
+              this.isUserLoggedIn=false;
+              
+          }
+          else{
+            this.isUserLoggedIn=true;
+            this.router.navigate(['/']);
+          }
+        }
+      }, false);
+    }
 
   logout(){
   this.router.navigate(['/login']);
